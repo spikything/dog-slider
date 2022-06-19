0306e87644153659b932e49e35a09293e98bd1d3
Liam O'Donnell <spikything@gmail.com>
2022-06-19 03:19:33 +0100

    Dogs animate in a bit with framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
 import './App.css';
 
@@ -17,13 +18,17 @@ function App() {
   return (
     <div className="App">
       <div className='row'>
-        <div className='container'>
-          {
-            dogs ? 
-            dogs.map((dog, index) => <img src={dog} key={index} alt='dog' />) 
-            : <p>Loading...</p>
-          }
-        </div>
+        <motion.div 
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1, left: '-30vw' }}
+          transition={{ type:'spring', stiffness:75, damping:8, delay:0.5 }}
+          className='container'>
+            {
+              dogs ? 
+              dogs.map((dog, index) => <img src={dog} key={index} alt='dog' />) 
+              : <p>Loading...</p>
+            }
+        </motion.div>
       </div>
     </div>
   );
