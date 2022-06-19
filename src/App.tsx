259258716188150f6b6f5f6d9b1a6855b437b39b
Liam O'Donnell <spikything@gmail.com>
2022-06-19 02:57:07 +0100

    Load random dog images

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [dogs, setDogs] = useState<string[] | null>(null);
+
+  useEffect(() => {
+    const fetchData = async (endpoint:string) => {
+      const result = await fetch(endpoint);
+      const json = await result.json();
+      setDogs(json.message);
+    }
+    fetchData('https://dog.ceo/api/breeds/image/random/5');
+  }, []);
+
   return (
     <div className="App">
+      <div className='row'>
+        <div className='container'>
+          {
+            dogs ? 
+            dogs.map((dog, index) => <img src={dog} key={index} alt='dog' />) 
+            : <p>Loading...</p>
+          }
+        </div>
+      </div>
     </div>
   );
 }
